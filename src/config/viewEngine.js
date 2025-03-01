07254bb6fffa6b3d3a2ceb657326c138174e11d7
dangthanhvinh003 <dvih003@gmail.com>
2025-03-01 10:11:59 +0700

    define route

---
+++ src/config/viewEngine.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const configViewEngine = (app) => {
+  //teamplate
+  app.set("views", path.join("./src", "views"));
+  app.set("view engine", "ejs");
+
+  //sttic files : css
+  app.use(express.static(path.join("./src", "public")));
+};
+
+module.exports = configViewEngine;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const port = 3000;
-const name = express.Router();
-const path = require("path");
+const configViewEngine = require("./config/viewEngine");
 
-const dbPath = path.join(__dirname, "../db.json");
-//jsonjson
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//config template
+configViewEngine(app);
 
-// congif Css src
-app.use(express.static(path.join(__dirname, "?")));
-
-name.route("/user").get((req, res) => {
-  const data = fs.readFileSync(dbPath, "utf8");
-  const jsonData = JSON.parse(data); // Chuyển đổi thành JSON
-  res.send(jsonData.product);
-});
-app.use(name);
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("sample.ejs");
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
